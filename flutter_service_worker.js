2e9ac740e12d4918ff53b6bc7a3b239feb3c3aa3
Carlos Cardoso <cvcardos5@gmail.com>
2024-02-14 15:23:39 -0300

    Built web
    Branch: main
    Commit: cb646ae0fac145a39335687bd862a8836f287693
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "14e24416c9bc25f31227f16fd917e647",
-"/": "14e24416c9bc25f31227f16fd917e647",
-"main.dart.js": "25e43bf374497c06ac8b0d561d9a8a26",
+"index.html": "10d3f1e04140b6a4fa6a0e2ea7db6e9f",
+"/": "10d3f1e04140b6a4fa6a0e2ea7db6e9f",
+"main.dart.js": "0d76138c8b30f52493a0faf9feb29725",
 "manifest.json": "72cedbe08125b1992411fe9d1de3ef30",
 "version.json": "ab7b79d6963f5ca5c6c1333e684bd677"};
 // The application shell files that are downloaded before a service worker can
