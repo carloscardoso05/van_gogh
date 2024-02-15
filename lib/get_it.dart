78ca072e374b8132bbbdba1a93ea148dacd3a37e
Carlos Cardoso <cvcardos5@gmail.com>
2024-02-14 21:50:39 -0300

    adicionado houses_controller ao get_it

--- lib/get_it.dart
+++ lib/get_it.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:van_gogh/controllers/houses_controller.dart';
 import 'package:van_gogh/repositories/holders_repository.dart';
 import 'package:van_gogh/repositories/houses_repository.dart';
 import 'package:van_gogh/repositories/payments_repository.dart';
@@ -8,12 +9,17 @@ final getIt = GetIt.instance;
 
 setupProviders() {
   getIt.registerSingleton<AuthService>(AuthService());
-  getIt.registerLazySingleton<HoldersRepository>(() => LocalHoldersRepository());
-  getIt.registerLazySingleton<PaymentsRepository>(() => LocalPaymentsRepository());
+  getIt
+      .registerLazySingleton<HoldersRepository>(() => LocalHoldersRepository());
+  getIt.registerLazySingleton<PaymentsRepository>(
+      () => LocalPaymentsRepository());
   getIt.registerLazySingleton<HousesRepository>(
     () => LocalHousesRepository(
       holdersRepository: getIt<HoldersRepository>(),
       paymentsRepository: getIt<PaymentsRepository>(),
     ),
+  );
+  getIt.registerLazySingleton<HousesController>(
+    () => HousesController(housesRepository: getIt<HousesRepository>()),
   );
 }
