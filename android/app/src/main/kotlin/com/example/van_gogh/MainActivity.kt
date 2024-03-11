6ad3aae40d92089ee04ddd519a197d4c20e0f24c
Carlos Cardoso <cvcardos5@gmail.com>
2024-03-11 19:37:54 -0300

    detalhes da casa

---
+++ android/app/src/main/kotlin/com/example/van_gogh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.van_gogh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/pages/house_details/house_details.dart
+++ lib/pages/house_details/house_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:van_gogh/entities/house.dart';
-import 'package:van_gogh/transformers/house_transformer.dart';
+import 'package:van_gogh/helpers/formating.dart';
+import 'package:van_gogh/helpers/payment_helpers.dart';
 
 class HouseDetails extends StatelessWidget {
   const HouseDetails({super.key, required this.house});
@@ -8,8 +9,37 @@ class HouseDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
-      child: Text(HouseTranformer.toJson(house).toString()),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Detalhes da casa")),
+      body: Column(
+        children: [
+          Text("Número: ${house.number}"),
+          Text("Quadra: ${house.block}"),
+          Text("Código: ${house.houseCode}"),
+          Text("Proprietário: ${house.holder ?? 'Ausente'}"),
+          Builder(
+            builder: (context) {
+              final payment = getMostDelayedPayment(house.payments);
+              if (payment == null) {
+                return const Text("Todas as contas em dias");
+              }
+              return Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Text(
+                          "Vencimento: ${extendedDateFormat.format(payment.dueDate)}"),
+                      Text(getPaymentStatePtBr(payment.state)),
+                      Text("Valor: R\$${numberFormat.format(payment.value)}")
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
